4737565565f336405b58a08c5db136317fc6e846
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-03-24 13:02:29 +0100

    Stuck with Can not find module post error

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,13 +1,13 @@
-require('./db');
+require('../db');
 const express = require('express');
 require('dotenv').config();
-// const morgan = require('morgan');
-const postRouter = require('./routers/post');
+const morgan = require('morgan');
+const postRouter = require('../routers/post');
 
 
 const app = express();
-// app.use(express.json());
-// app.use(morgan('dev'));
+app.use(express.json());
+app.use(morgan('dev'));
 app.use('/api/post', postRouter);
 
 const PORT = process.env.PORT;
